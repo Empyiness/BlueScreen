022f038d919e742e00e388caaed4fc11c8faad33
Admin <Admin@DESKTOP-NHMAQ26>
2022-10-13 22:07:05 +0300

    Добавьте файлы проекта.

---
+++ BlueScreen/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlueScreen
+{
+    public partial class MainWindow : Window
+    {
+        ImageBrush background = new ImageBrush();
+        public MainWindow()
+        {
+            InitializeComponent();
+            background.ImageSource = BitmapConverter.ImageSourceFromBitmap(Properties.Resources.image);
+            Window1.Background = background;
+        }
+
+        private void Close(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow Window1 = new MainWindow();
+            Window1.Show();
+        }
+    }
+}
